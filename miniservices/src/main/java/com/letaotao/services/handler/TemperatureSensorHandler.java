72e79ad829d7dbe60b9bec51b96f6e5eedae7b79
YanzheF <awesomeyanzhe@gmail.com>
2021-10-29 21:15:31 -0700

    Update TemperatureSensorHandler.java

--- miniservices/src/main/java/com/letaotao/services/handler/TemperatureSensorHandler.java
+++ miniservices/src/main/java/com/letaotao/services/handler/TemperatureSensorHandler.java
@@ -1,6 +1,8 @@
 package com.letaotao.services.handler;
 
+import com.letaotao.common.utils.LogUtil;
 import com.letaotao.services.model.TemperatureSensorModel;
+import com.letaotao.services.partners.twilio.TwilioService;
 import com.letaotao.services.repository.TemperatureSensorsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
@@ -19,6 +21,20 @@ public class TemperatureSensorHandler {
     protected TemperatureSensorsRepository temperatureSensorsRepository;
 
     private static final Logger LOGGER = LogManager.getLogger(TemperatureSensorHandler.class);
+
+    public void notificationCheck(String target, String ambient){
+        LogUtil.info(log, "notificationCheck, target : "+target +" and the ambient: "+ambient);
+
+        if (Float.parseFloat(target) - Float.parseFloat(ambient) >= 10 || Float.parseFloat(target) >= 37){
+            TwilioService.smsAlert(target, ambient);
+            LogUtil.info(log, "sms notificationCheck sent");
+            TwilioService.callOutAlert(target, ambient);
+            LogUtil.info(log, "callout notificationCheck sent");
+        }else{
+            LogUtil.info(log, "looks normal no need to notify");
+        }
+
+    }
 
     public void createTemperatureRecord(String target, String ambient, String timeCollectedAt,  String comments){
 
